b303fe25b2a82feaafce542816a232751aebb163
Aryan Parmar <W0848380@myscc.ca>
2025-07-09 11:46:36 -0400

    Completed assignment6

--- app/src/main/java/com/example/assignment5k/MainActivity.kt
+++ app/src/main/java/com/example/assignment5k/MainActivity.kt
@@ -1,30 +1,55 @@
 package com.example.assignment5k
-
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
+    class Expense(val name: String, val amount: Double)
+
+    private lateinit var nameInput: EditText
+    private lateinit var amountInput: EditText
+    private lateinit var addButton: Button
+    private lateinit var recyclerView: RecyclerView
+
+    private val expenseList = ArrayList<Expense>()
+    private lateinit var adapter: ExpenseAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val enterName: EditText = findViewById(R.id.enterName)
-        val btnShowName: Button = findViewById(R.id.btnShowName)
-        val Message: TextView = findViewById(R.id.Message)
+        nameInput = findViewById(R.id.nameInput)
+        amountInput = findViewById(R.id.amountInput)
+        addButton = findViewById(R.id.addButton)
+        recyclerView = findViewById(R.id.recyclerView)
 
-        btnShowName.setOnClickListener {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ExpenseAdapter(expenseList)
+        recyclerView.adapter = adapter
 
-            val name = enterName.text.toString()
+        addButton.setOnClickListener {
+            val name = nameInput.text.toString()
+            val amountText = amountInput.text.toString()
 
-            if (name.isNotEmpty()) {
-                Message.text = "Hello, $name!"
+            if (name.isEmpty() || amountText.isEmpty()) {
+                Toast.makeText(this, "Please enter name and amount", Toast.LENGTH_SHORT).show()
             } else {
-                Message.text = "Please enter your name."
+                val amount = amountText.toDoubleOrNull()
+                if (amount == null) {
+                    Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_SHORT).show()
+                } else {
+                    val newExpense = Expense(name, amount)
+                    expenseList.add(newExpense)
+                    adapter.notifyItemInserted(expenseList.size - 1)
+
+                    nameInput.text.clear()
+                    amountInput.text.clear()
+                    Toast.makeText(this, "Expense Added", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
